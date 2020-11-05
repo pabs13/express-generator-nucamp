5110b4527c3820cb1523c3f336238370a9e5f6a2
Pablo Salce <salcepablo13@gmail.com>
2020-11-05 13:56:02 -0800

    File Upload

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const partnerRouter = require('./routes/partnerRouter');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const config = require('./config');
+const uploadRouter = require('./routes/uploadRouter');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -55,6 +56,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
