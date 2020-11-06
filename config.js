988bc9d24c546191e7db6dde188e29b5b70af6a2
Pablo S <50646101+pabs13@users.noreply.github.com>
2020-11-06 14:07:36 -0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '658852411658995',
-        clientSecret: 'e403acfc7934b54cd91c001d8663be23'
-    }
-}
